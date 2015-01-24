ab7048812cdb48f068b896d90a49afd0a1cf842a
ravitejas <ravi.teja.sanampudi@gmail.com>
2015-01-24 14:16:09 -0800

    spirit player code

---
+++ Game/Assets/Scripts/SpiritCharacterScripts/SpiritPlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpirtPlayerMovement : MonoBehaviour {
+	
+	public float MOVEMENT_SPEED = 20;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float horInput = Input.GetAxis("HorizontalController");
+		float verInput = Input.GetAxis("VerticalController");
+		
+		Vector3 input = new Vector3(horInput, verInput, 0);
+		transform.Translate(MOVEMENT_SPEED * input * Time.deltaTime); 
+	}
+}
+
