cdec5ac78c20567c6a761864c63ae8fa0a5968d0
Tonaplo <tonaplo@msn.com>
2015-01-24 13:49:32 -0800

    Jumping implemented

--- Game/Assets/Scripts/PhysicalCharacterScripts/PhysicalPlayerMovement.cs
+++ Game/Assets/Scripts/PhysicalCharacterScripts/PhysicalPlayerMovement.cs
@@ -3,16 +3,30 @@ using System.Collections;
 
 public class PhysicalPlayerMovement : MonoBehaviour {
 
-    float movementSpeed = 5f;
+    public float movementSpeed = 5f;
+    public float jumpForce = 200f;
+
+    bool isJumping = false;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Awake () {
+	    
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        Vector3 input = new Vector3(Input.GetAxis("Horizontal"),0,0);
-        transform.Translate(movementSpeed * input * Time.deltaTime); 
+	void FixedUpdate () {
+
+        Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+        transform.Translate(movementSpeed * input * Time.deltaTime);
+        if (Input.GetAxis("Vertical") > 0f && !isJumping)
+        {
+            rigidbody2D.AddForce(new Vector2(0, jumpForce));
+            isJumping = true;
+        }
 	}
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        isJumping = false;
+    }
 }
